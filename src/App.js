4cf65071aa491ee55fa8802da3d13d0e9827e5c0
turhanomer <omerturhan16@outlook.com>
2024-08-26 20:10:24 +0300

    added to icons

--- src/App.js
+++ src/App.js
@@ -31,10 +31,10 @@ function App() {
   };
 
   const handleDelete = (index) => {
-    const newSavedTimes = savedTimes.filter((_,i) => i !== index);
+    const newSavedTimes = savedTimes.filter((_, i) => i !== index);
     setSavedTimes(newSavedTimes);
     localStorage.setItem("savedTimes", JSON.stringify(newSavedTimes));
-  }
+  };
 
   return (
     <>
@@ -58,31 +58,64 @@ function App() {
           <span>{("0" + (Math.floor(time / 1000) % 60)).slice(-2)}:</span>
           <span>{("0" + (Math.floor(time / 10) % 100)).slice(-2)}</span>
         </div>
-        <div className="w-1/4 max-w-sm flex flex-row justify-between">
+        <div id="buttons" className="w-1/2 flex justify-between">
           {running ? (
             <button
-              className="border rounded-lg py-1 px-3"
+              className="border rounded-lg py-2 px-9 flex items-center gap-2"
               onClick={handleStop} // handleStop'u doğrudan bağla
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                className="bi bi-pause-circle"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                <path d="M5 6.25a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0zm3.5 0a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0z" />
+              </svg>
               Stop
             </button>
           ) : (
             <button
-              className="border rounded-lg py-1 px-3 hover:bg-sky-950"
+              className="border rounded-lg py-2 px-9 hover:bg-sky-950 flex items-center gap-2"
               onClick={() => {
                 setRunning(true);
               }}
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                className="bi bi-skip-start-circle "
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                <path d="M10.229 5.055a.5.5 0 0 0-.52.038L7 7.028V5.5a.5.5 0 0 0-1 0v5a.5.5 0 0 0 1 0V8.972l2.71 1.935a.5.5 0 0 0 .79-.407v-5a.5.5 0 0 0-.271-.445" />
+              </svg>
               Start
             </button>
           )}
           <button
-            className="border rounded-lg py-1 px-3 hover:bg-sky-950"
+            className="border rounded-lg py-2 px-9 hover:bg-sky-950 flex items-center gap-2"
             onClick={() => {
               localStorage.removeItem("savedTimes");
               setSavedTimes([]);
             }}
           >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              className="bi bi-x-circle"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+            </svg>
             Reset
           </button>
         </div>
@@ -94,20 +127,21 @@ function App() {
               <div key={index} className="saved-time-entry">
                 Saved time: {index + 1} <br />
                 <div>
-                <span>
-                  {("0" + (Math.floor(savedTime / 60000) % 60)).slice(-2)}:
-                </span>
-                <span>
-                  {("0" + (Math.floor(savedTime / 1000) % 60)).slice(-2)}:
-                </span>
-                <span>
-                  {("0" + (Math.floor(savedTime / 10) % 100)).slice(-2)}
-                </span>
+                  <span>
+                    {("0" + (Math.floor(savedTime / 60000) % 60)).slice(-2)}:
+                  </span>
+                  <span>
+                    {("0" + (Math.floor(savedTime / 1000) % 60)).slice(-2)}:
+                  </span>
+                  <span>
+                    {("0" + (Math.floor(savedTime / 10) % 100)).slice(-2)}
+                  </span>
                 </div>
                 <button
-                onClick={() => handleDelete(index)}   
+                  onClick={() => handleDelete(index)}
                   type="button"
-                  className="text-white btn btn-outline-danger float-end items-center px-3 -my-11">
+                  className="text-white btn btn-outline-danger float-end items-center px-3 -my-11"
+                >
                   Delete
                 </button>
               </div>
